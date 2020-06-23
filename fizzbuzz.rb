e4b5314e00d6ba6fe8d509bb39502a98cea1a1bf
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-06-23 15:27:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,6 @@
 # See README.md for instructions on how to do this
 require_relative './spec_helper.rb'
 
-def fizzbuzz(item)
   def fizzbuzz(num)
     if num % 3 == 0 && num % 5 == 0
       “FizzBuzz”
